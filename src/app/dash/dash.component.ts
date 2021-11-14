071a92bf72a5a618e0a5f3fdbe319c28ba659204
Eric Yan <eyanric@gmail.com>
2021-11-14 16:16:20 -0500

    added scuffed af text editor

--- src/app/dash/dash.component.ts
+++ src/app/dash/dash.component.ts
@@ -4,6 +4,7 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 import { StoreSummaryService } from '../store-summary/store-summary.service';
 import { StoreSummary } from '../store-summary/store-summary';
 import { CdkDragEnd, CdkDragEnter, moveItemInArray } from '@angular/cdk/drag-drop';
+import * as Quill from 'quill';
 
 @Component({
   selector: 'app-dash',
@@ -42,6 +43,7 @@ export class DashComponent implements OnInit{
   constructor(private breakpointObserver: BreakpointObserver, private summaryService: StoreSummaryService) {}
 
   ngOnInit() {
+    hello();
     this.summaryService.getStoreSummary().subscribe({
       next: summaryData => {
         this.miniCardData = summaryData;
@@ -49,3 +51,31 @@ export class DashComponent implements OnInit{
     });
   }
 }
+function hello() {
+  var toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+
+    ['clean']                                         // remove formatting button
+  ];
+  var fullEditor = new Quill('#full-editor', {
+  modules: {
+    'toolbar': toolbarOptions,
+    'link-tooltip': true
+  },
+  theme: 'snow'
+  });
+}
+
---
+++ src/texteditor.js
@@ -0,0 +1,30 @@
+(function hello() {
+  var toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+
+    ['clean']                                         // remove formatting button
+  ];
+  var fullEditor = new Quill('#full-editor', {
+  modules: {
+    'toolbar': toolbarOptions,
+    'link-tooltip': true
+  },
+  theme: 'snow'
+  });
+})()
+
+
+//fullEditor.addModule('toolbar', {container: '#full-toolbar'});
